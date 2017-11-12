544fb26bdc3c29f369d94c4852dbeccc686d8095
training@hashiba.org <training@hashiba.org>
2017-11-12 05:45:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function scuberGreetingForFeet(feet){
   if (feet <= 400){
 
     return "This one is on me!";
+  } else if (feet > 4000) {
+
+    return 'I will gladly take your thirty bucks.';
+    
   }
-  
+
 }
