fbb2801bcbcbc7e88d72020d51e212c457b46ca0
training@hashiba.org <training@hashiba.org>
2017-11-12 05:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ if (city === 'NYC') {
 function switchOnCharmFromTip(tip){
 if (tip === 'generous') {
   return "Thank you so much.";
-}else if (tip === 'not as generous') {
+}
+else if (tip === 'not as generous') {
   return "Thank you.";
-
-
+}
+else {
+  return "Bye.";
 }
 
 }
-
-
