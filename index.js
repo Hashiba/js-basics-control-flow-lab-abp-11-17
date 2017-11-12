ea59f0d2c13ad20b6cb2572c90c8e29e918702a2
training@hashiba.org <training@hashiba.org>
2017-11-12 05:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ function ternaryCheckCity(city){
 if (city === 'NYC') {
 
   return "Ok, sounds good.";
-  
+
 } else if (city !== 'NYC') {
 
   return "No go.";
+
+}
+
+}
+
+function switchOnCharmFromTip(tip){
+if (tip === 'generous') {
+  return "Thank you so much.";
+}else if (tip === 'not as generous') {
+  return "Thank you.";
+else {
+  return "Bye.";
+}
   
 }
   
